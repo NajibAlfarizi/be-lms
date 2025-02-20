ebad2f01c8e5f0b461c5e90400810efa803e07a5
Jibalfa <najibalfarizi5@gmail.com>
2025-02-20 12:43:43 +0700

    implement payment processing with Midtrans in sign-up functionality

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,8 +1,13 @@
 import bcrypt from "bcryptjs";
 import userModel from "../models/userModel.js";
+import TransactionModel from '../models/transactionModel.js';
 import { StatusCodes } from "http-status-codes";
 
 export const signUp = async (req, res) => {
+  const midtransUrl = process.env.MIDTRANS_URL;
+  const midtransAuthString = process.env.MIDTRANS_AUTH_STRING;
+  const midtransCallBackSuccess = process.env.MIDTRANS_CALLBACK_SUCCESS;
+
   try {
     const body = req.body;
 
@@ -17,13 +22,43 @@ export const signUp = async (req, res) => {
     }) 
 
     // payment midtrans
+    const transaction = new TransactionModel({
+      user: user._id,
+      price: 280000,
+    })
+
+    const midtrans = await fetch(midtransUrl, {
+      method: "POST",
+      body: JSON.stringify({
+        transaction_details: {
+          order_id: transaction._id.toString(),
+          gross_amount: transaction.price,
+        },
+        credit_card: {
+          secure: true,
+        },
+        customer_details: {
+          email: user.email,
+        },
+        callbacks: {
+          finish: "http://localhost:5173/success-checkout",
+        },
+      }),
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Basic ${midtransAuthString}`,
+      },
+    });
+
+    const resMidtrans = await midtrans.json();
 
     await user.save();
+    await transaction.save();
 
     return res.status(StatusCodes.CREATED).json({
       message: "Sign up success",
       data: {
-        midtrans_url: "https://app.sandbox.midtrans.com/snap/v1/transactions",
+        midtrans: resMidtrans.redirect_url,
       }
     })
   } catch (error) {
